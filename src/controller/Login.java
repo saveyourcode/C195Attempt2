80acff8f138024ac014af7e1387d713027c50e1d
savemycode <kjac790@wgu.edu>
2022-06-16 11:26:55 -0700

    Initial commit

---
+++ src/controller/Login.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private TextField loginPasswordText;
+
+    @FXML
+    private Label loginRegionCodeText;
+
+    @FXML
+    private TextField loginUsernameText;
+
+    @FXML
+    void onActionLoginButton(ActionEvent event) {
+
+    }
+}
