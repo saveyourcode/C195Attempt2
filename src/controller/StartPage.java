41ac5631b467809ecfabbea678ad17432683f999
savemycode <kjac790@wgu.edu>
2022-06-16 12:36:13 -0700

    6/16/2022 commit 1

---
+++ src/controller/StartPage.java
@@ -0,0 +1,126 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class StartPage {
+
+    @FXML
+    private TableView<?> startPageAppointmentsTableView;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewAppointmentsCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewContactCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewCustomerIDCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewDescriptionCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewEndingCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewLocationCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewStartingCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewTitleCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewTypeCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageAppointmentsTableViewUserIDCol;
+
+    @FXML
+    private TableView<?> startPageCustomersTableView;
+
+    @FXML
+    private TableColumn<?, ?> startPageCustomersTableViewAddressCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageCustomersTableViewCountryCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageCustomersTableViewCustomerIDCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageCustomersTableViewCustomerNameCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageCustomersTableViewDivisionCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageCustomersTableViewPhoneNumberCol;
+
+    @FXML
+    private TableColumn<?, ?> startPageCustomersTableViewPostalCodeCol;
+
+    @FXML
+    void onActionStartPageAllRadioButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageAppointmentsAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageAppointmentsDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageAppointmentsModify(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageCustomerAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageCustomerDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageCustomerModify(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageMonthRadioButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageReports(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStartPageWeekRadioButton(ActionEvent event) {
+
+    }
+}
